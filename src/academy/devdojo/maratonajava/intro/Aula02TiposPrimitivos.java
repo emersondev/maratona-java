536ea657498981be628a67d6647bf2456daa031f
emersondev <emersonnovais93@gmail.com>
2024-07-03 21:04:19 -0300

    Aula 12
    Signed-off-by: emersondev <emersonnovais93@gmail.com>

---
+++ src/academy/devdojo/maratonajava/intro/Aula02TiposPrimitivos.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.intro;
+
+public interface Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean
+        int age = 10;
+        double salaryDouble = 2000;
+        float salaryFloat = 2500;
+        byte ageByte = 10;
+        short ageShort = 10;
+        boolean tr = true;
+        boolean fl = false;
+        char character = 10;
+
+        System.out.println("A idade é: " + age);
+    }
+}
